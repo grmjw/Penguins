c63f64f984d4afe62df8c0ff2c91659c7d840915
Yusef Ahmed <yusefahmed0403@gmail.com>
2024-05-08 13:00:11 +0200

    Added an example workspace as a demo

---
+++ example_workspace/build/my_package/CMakeFiles/my_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_node.dir/src/my_node.cpp.o"
+  "my_node"
+  "my_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
